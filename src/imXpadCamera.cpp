6b37c62e2977db04f6c5d8fa5f485ad96b87be86
Guillaume DI FRANCO (THALES) <guillaume.di-franco@synchrotron-soleil.fr>
2021-07-07 15:39:49 +0200

    [PROBLEM-1857][ImXPAD]Remove unused code

--- src/imXpadCamera.cpp
+++ src/imXpadCamera.cpp
@@ -348,7 +348,6 @@ void Camera::getStatus(XpadStatus& status)
 	unsigned short pos;
 	cmd << "GetDetectorStatus";
 
-	//m_xpad_alt->sendWait(cmd.str(), str);
 	m_xpad_alt->sendCustomWait(cmd.str(), str);
 	pos = str.find(".");
 	string state = str.substr (0, pos);
